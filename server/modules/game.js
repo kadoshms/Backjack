8f6bfdd0877db060c0ffa5d9966ccfd7a2f6ec94
mor <kadoshms@gmail.com>
2015-07-13 01:18:24 +0300

    Implemented simple game manager

---
+++ server/modules/game.js
@@ -0,0 +1,22 @@
+/**
+*	File Name 	: server/modules/game
+*	Description	: this module holds all game operations
+*	Author		: kadoshms
+*/
+
+/**
+ * Construct a new game manager
+ * @param {socket.io} io object
+ */
+function Game()
+{
+}
+
+/**
+ * Hit player
+ */
+Game.prototype.hit = function(player){
+
+}
+
+module.exports = Game;
--- server/server.js
+++ server/server.js
@@ -5,11 +5,13 @@ var winston 		= 	require('winston');
 var Netwokring	 	=	require('./modules/networking');
 var Player			=	require('./modules/player');
 var Room			=	require('./modules/room');
+var Game			=	require('./modules/game');
 
 // Create the roomm
 var room = new Room("Las Vegas");
 
 var networking = new Netwokring(io);
+var game 	   = new Game();
 
 // Handle basic connection
 io.on("connection", function(socket){
@@ -26,6 +28,14 @@ io.on("connection", function(socket){
 		room.playerJoin(player, socket);
 
 		networking.toRoom(room, "playerJoined", { room : room });
+	});
+
+	/**
+	 * Handle user action
+	 */
+	socket.on("playerAction", function(action){
+		var player = room.getPlayer(socket.id);
+		game[action].apply(player)
 	});
 });
 
