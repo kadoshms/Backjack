049dfc518b69c1950d40bf00839de00d2342c715
mor <kadoshms@gmail.com>
2015-10-06 15:05:27 +0300

    added player ready button to controls

--- client/js/views/main.js
+++ client/js/views/main.js
@@ -7,24 +7,26 @@
 define([
         'jquery',
         'backbone',
-        'socket.io'
+        'socket.io',
+        'views/controls'
 ],
-function($, Backbone, io){
+function($, Backbone, io, Controls){
 
 	var exports = {};
 	
 	exports.MainView = Backbone.View.extend({
 		el		:	'#main-content',
-		events	: 	{
-			'click' : function(){
-				io.socket.emit("playerAction", "hit");
-			}
-		},
+		events	: 	{},
 		/**
 		 * Sets up graphics for new player
 		 */
 		initRoom:	function(data){
 			var players = data.room.players;
+			var player	= data.player;
+
+			// render controls
+			var controls = new Controls.ControlsView();
+			this.$el.append(controls.render().el);
 		},
 		// handle player actions
 		/**
@@ -32,6 +34,15 @@ function($, Backbone, io){
 		 * @param {object} data retrived from server
 		 */
 		hit:	function(data){
+			this.$el.append(data.result);
+		},
+		/**
+		 * Handle player ready (after server is done handling request)
+		 * @param {object} data retrived from server
+		 */
+		ready: function(data){
+			// the ready button is useles after it has been used
+			this.$el.find('#playerReady').remove();
 		},
 		/**
 		 * Render the view
