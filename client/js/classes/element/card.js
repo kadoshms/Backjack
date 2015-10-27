60a2fe50c2754b487f7868972c2a76c649538689
mor <kadoshms@gmail.com>
2015-10-27 08:41:20 +0200

    added card tween & position formatter

--- client/js/classes/element/card.js
+++ client/js/classes/element/card.js
@@ -33,7 +33,25 @@ define([
 		var fileName = this.flipped ? "b2fv" : this.name;
 		var container = stage.getContainerByPosition(player.sit);
 
-		stage.addBitmap("client/image/classic-cards/"+fileName+".png", container);
+		// position maniuplation method
+		// changes card offest regarding its position
+		var posFunc = function(){
+			var x = _card.indexInHand * 15;
+			var y = 0;
+			return {x : x , y : y};
+		};
+
+		// tween function
+		var tweenFunc = function(card){
+			if(animate === false) return;
+			card.y = -500;
+			card.alpha = 0;
+			createjs.Tween.get(card, {loop: false}, null, false)
+				.to({y : 0, alpha : 1}, 1000);
+		}
+
+		var options = { alpha : (animate === false) ? 1 : 0 };
+		stage.addBitmap("client/image/classic-cards/"+fileName+".png", container, posFunc, tweenFunc, options, (this.indexInHand-1));
 	}
 
 	return Card;
